921e6a1a99f66adbe844e7918e75652594897032
Ubuntu <ubuntu@domU-12-31-39-03-AA-2D.compute-1.internal>
2013-07-19 22:53:15 +0000

    1st attempt pt7

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var indexbuf = fs.readFileSync("index.html");
+var indexstr = indexbuf.toString();
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(indexstr);
 });
 
 var port = process.env.PORT || 5000;
